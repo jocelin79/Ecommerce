67621b27423ddc9ca6b237e11ba3dc0ba3ac2398
Jocelin Nunes <jocelinnunes@gmail.com>
2022-05-23 14:12:50 -0300

    Register and Login pages were created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 
 function App() {
   return (
-    <Register/>
+    <Login/>
   );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,53 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-  
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+    rgba(255, 255, 255, 0.5),
+    rgba(255, 255, 255, 0.5)
+    ), url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: #fff;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0 0;
+  padding: 10px;
+`
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0;
+`
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
 `
 
 const Register = () => {
@@ -9,7 +55,7 @@ const Register = () => {
     <Container>
       <Wrapper>
         <Title>CREATE AN ACCOUNT</Title>
-        <From>
+        <Form>
           <Input placeholder="name"/>
           <Input placeholder="last name"/>
           <Input placeholder="username"/>
@@ -18,7 +64,7 @@ const Register = () => {
           <Input placeholder="confirm password"/>
           <Agreement>By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVACY POLICY</b></Agreement>
           <Button>CREAT</Button>
-        </From>
+        </Form>
       </Wrapper>
     </Container>
   )

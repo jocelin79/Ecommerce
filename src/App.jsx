0bff180599266922b8d07501774269870caa8f39
Jocelin Nunes <jocelinnunes@gmail.com>
2022-05-27 16:22:00 -0300

    React Redux Toolkit Authentification created

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import Success from "./pages/Success";
+import { useSelector } from "react-redux";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
+
 // import { useSelector } from "react-redux";
 
 const App = () => {
-  const user = true
+  const user = useSelector(state => state.user.currentUser)
   // useSelector((state) => state.user.currentUser);
   return (
     <Router>
